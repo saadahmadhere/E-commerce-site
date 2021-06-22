0d6b81c1f74edc15d31733c77f4a7bf67697ee9e
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-23 00:51:08 +0530

    add + and - button action

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 
 const CartItem = () => {
+    const [quantity, setQuantity] = useState(1);
+
+    const addButtonHandler = () =>{
+        setQuantity(quantity+1);
+    }
+
+    const removeButtonHandler = () =>{
+        setQuantity(quantity-1);
+    }
 
     return (
         <div className="cart-item">
             <div className="right-block">
-                <img className = "product-image"  alt="Product Image" />
+                <img className = "product-image"  alt="Product" />
             </div>
             <div className="left-block">
                 <div>Phone</div>
                 <div>Rs.999</div> 
-                <div>Qty: 1</div>
+                <div>Qty: {quantity}</div>
                 <div className="cart-item-buttons">
-                   <img className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" alt="" />
-                   <img className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="minus sign" />
+                   <img onClick = {addButtonHandler} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" alt="" />
+                   <img onClick = {removeButtonHandler} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="minus sign" />
                    <img className = "action-icons" src="https://image.flaticon.com/icons/png/512/3096/3096673.png" alt="delete icon" />
                 </div>
             </div>
