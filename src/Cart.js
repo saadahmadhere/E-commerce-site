682558bc16da3fb30122384e0ddd8e05a130c2b4
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-06-27 02:53:09 +0530

    set inc, dec, and delete functionality

--- src/Cart.js
+++ src/Cart.js
@@ -1,10 +1,62 @@
+import { useState } from 'react'
 import CartItem from './CartItem'
 
 const Cart = () => {
+    
+    const [products, setProducts] = useState(
+             [
+            {
+            title: "Mobile",
+            qty: 100,
+            price: 9999,
+            id: 1
+            },
+            {
+            title: "Laptop",
+            qty: 5,
+            price: 99999,
+            id:2
+            },
+            {
+            title: "iPad",
+            qty: 10,
+            price: 90999,
+            id:3
+            }]
+    )
+
+        const handleIncreaseQuantity = (product) =>{
+                const index = products.indexOf(product)
+                products[index].qty += 1; 
+                setProducts([...products])
+        }
+
+        const handleDecreaseQuantity = (product) =>{
+            const index = products.indexOf(product)
+
+            if(products[index].qty > 0){
+                products[index].qty -= 1; 
+                setProducts([...products])
+            }
+            
+    }
+
+        const handleDeleteQuantity = (product) =>{
+             const productId = product.id
+             const newProducts = products.filter((item) => (item.id !== productId))
+             setProducts(newProducts)
+        }
+
     return (
         <div>
-            <CartItem/>
-            <CartItem/>
+            {products.map((product) => 
+               (<CartItem product = {product}
+                onIncreaseQty = {handleIncreaseQuantity}
+                onDecreaseQty = {handleDecreaseQuantity}
+                onDeleteQty = {handleDeleteQuantity}
+                key={product.id}/>)
+            )}
+         
         </div> );
 }
  
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,40 +1,19 @@
-import { useState } from "react";
 
-const CartItem = () => {
-    const [itemDetails, setItemDetails] = useState({
-        title: "Phone",
-        qty: 1,
-        price: 999
-    });
-
-    const addButtonHandler = () =>{
-        setItemDetails(prevState => {
-            return {...prevState, qty: prevState.qty +1}
-        });
-
-    }
-
-    const removeButtonHandler = () =>{
-        if(itemDetails !== 0){
-            setItemDetails(prevState => {
-                return {...prevState, qty: prevState.qty -1}
-            });
-        }
-    }
-
+const CartItem = (props) => {
+  
     return (
         <div className="cart-item">
             <div className="right-block">
                 <img className = "product-image"  alt="Product" />
             </div>
             <div className="left-block">
-                <h3>{itemDetails.title}</h3>
-                <div style = {{color: '#777'}}>Rs.{itemDetails.price}</div> 
-                <div style = {{color: '#777'}}>Qty: {itemDetails.qty}</div>
+                <h3>{props.product.title}</h3>
+                <div style = {{color: '#777'}}>Rs.{props.product.price}</div> 
+                <div style = {{color: '#777'}}>Qty: {props.product.qty}</div>
                 <div className="cart-item-buttons">
-                   <img onClick = {addButtonHandler} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" alt="" />
-                   <img onClick = {removeButtonHandler} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="minus sign" />
-                   <img className = "action-icons" src="https://image.flaticon.com/icons/png/512/3096/3096673.png" alt="delete icon" />
+                   <img onClick = {() => props.onIncreaseQty(props.product)} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992651.png" alt="" />
+                   <img onClick = {() => props.onDecreaseQty(props.product)} className = "action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" alt="minus sign" />
+                   <img onClick = {() => props.onDeleteQty(props.product)} className = "action-icons" src="https://image.flaticon.com/icons/png/512/3096/3096673.png" alt="delete icon" />
                 </div>
             </div>
         </div>
